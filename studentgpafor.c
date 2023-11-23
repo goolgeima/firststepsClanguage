59176683c85265481a5ed9883a58a6c6bf58dda5
goolgeima <151480220+goolgeima@users.noreply.github.com>
2023-11-22 21:50:22 -0300

    Update and rename alunomediafor.c to studentgpafor.c

---
+++ studentgpafor.c
@@ -0,0 +1,16 @@
+int main(){
+    int num, counter;
+    float gpa;
+    for(counter=1; counter<6; counter++){
+        printf("Type the number and the grade of the student %d\n", contador);
+        scanf("%d", &num);
+        scanf("%f", &gpa);
+        if(gpa>=7){
+            printf("The student has been approved");
+        }
+        else{
+            printf("The student has been reproved"\n);
+        }
+    }
+
+}
