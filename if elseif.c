08f801a359172e64f71dfde28d20524879542240
Gustavo Zembruski <151480220+goolgeima@users.noreply.github.com>
2023-11-22 22:16:13 -0300

    Update if elseif.c

--- if elseif.c
+++ if elseif.c
@@ -1,24 +1,24 @@
 int main(){
     float num1;
     float num2;
-    int operacao;
+    int op;
     do{
-     printf("\n \tDigite dois numeros: ");
+     printf("\n \tDType two numbers: ");
      scanf("%f%f", &num1, &num2);
-     printf("Qual operacao quer fazer?\n1)Soma\n2)Subtracao\n3)Multiplicacao\n4)Divisao\n5)Sair\n \t");
-     scanf("%d", &operacao);
-        if(operacao==1){
-            printf("resposta: %f", num1+num2);
+     printf("Which operation do you want to do?\n1)Sum\n2)Subtraction\n3)Multiplication\n4)Division\n5)Leave\n \t");
+     scanf("%d", &op);
+        if(op==1){
+            printf("The answer is: %f", num1+num2);
         }
-        else if(operacao==2){
-            printf("resposta: %f", num1-num2);
+        else if(op==2){
+            printf("The answer is: %f", num1-num2);
         }
-        else if (operacao==3){
-            printf("resposta: %f", num1*num2);
+        else if (op==3){
+            printf("The answer is: %f", num1*num2);
         }
-        else if(operacao==4){
-            printf("resposta: %f", num1/num2);
+        else if(op==4){
+            printf("The answer is: %f", num1/num2);
         }
-    }while (operacao!=5);
+    }while (op!=5);
     return 0;
 }
