7bd79c60fa3ecd15b6721fc6810769260b9a38ec
goolgeima <151480220+goolgeima@users.noreply.github.com>
2023-11-22 21:42:39 -0300

    Update and rename numerospares.c to pairnumbers.c

---
+++ pairnumbers.c
@@ -0,0 +1,12 @@
+int main(){
+    int numbers = 0;
+    int divisor = 2;
+    int rest;
+    while(numbers<20){
+        rest=(numbers % divisor);
+        if(resto==0){
+        printf("%d\n", numbers);
+    }
+    numbers++;
+    }
+}
